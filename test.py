b10eaedd3865f868db36102ead7b9aea245c3666
wangruijiedsg <91312405+wangruijiedsg@users.noreply.github.com>
2023-11-22 14:07:23 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+client = OpenAI()
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "地球周长是多少？"}
+  ]
+)
+
+print(completion.choices[0].message)
